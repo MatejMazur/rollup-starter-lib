3c2b9e3206f1d20b3a6e304a6272b3e332f06197
Matej Mazur <matej.mazur@mail.com>
2018-07-26 15:45:23 +0200

    Change the code purpose and update configs (rollup, babel)

--- rollup.config.js
+++ rollup.config.js
@@ -4,37 +4,46 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
-	{
-		entry: 'src/main.js',
-		dest: pkg.browser,
-		format: 'umd',
-		moduleName: 'howLongUntilLunch',
-		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs(), // so Rollup can convert `ms` to an ES module
-			babel({
-				exclude: ['node_modules/**']
-			})
-		]
-	},
+  // browser-friendly UMD build
+  {
+    input: pkg.source,
+    output: {
+      name: transformToCamelCase(pkg.name),
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      babel({
+        exclude: ['node_modules/**']
+      })
+    ]
+  },
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// the `targets` option which can specify `dest` and `format`)
-	{
-		entry: 'src/main.js',
-		external: ['ms'],
-		targets: [
-			{ dest: pkg.main, format: 'cjs' },
-			{ dest: pkg.module, format: 'es' }
-		],
-		plugins: [
-			babel({
-				exclude: ['node_modules/**']
-			})
-		]
-	}
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: pkg.source,
+    external: Object.keys(pkg.peerDependencies),
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ]
+  }
 ];
+
+function transformToCamelCase(name) {
+  // transform "@org-name/package-name" to "package-name"
+  const lispCase = name.replace(/.*\//, '');
+  const [firstWord, ...words] = lispCase.split('-');
+  const camelCase = [
+    firstWord,
+    ...words.map(word => word.charAt(0).toUpperCase() + word.substr(1))
+  ].join('');
+  return camelCase;
+}
